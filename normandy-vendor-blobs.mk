61921a5b233e54fe86ca035fa96e004ab8323ed0
dhacker29 <dhackerdvm@gmail.com>
2014-04-17 05:34:25 -0400

    Build ril
    Change-Id: Ie5a6c6150a869b2f5ca492ac9e37d4882c6a68bb

--- normandy-vendor-blobs.mk
+++ normandy-vendor-blobs.mk
@@ -35,7 +35,6 @@ PRODUCT_COPY_FILES += \
     vendor/nokia/normandy/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/nokia/normandy/proprietary/bin/qosmgr:system/bin/qosmgr \
     vendor/nokia/normandy/proprietary/bin/radish:system/bin/radish \
-    vendor/nokia/normandy/proprietary/bin/rild:system/bin/rild \
     vendor/nokia/normandy/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/nokia/normandy/proprietary/bin/sapd:system/bin/sapd \
     vendor/nokia/normandy/proprietary/bin/subsystem_ramdump:system/bin/subsystem_ramdump \
@@ -132,7 +131,6 @@ PRODUCT_COPY_FILES += \
     vendor/nokia/normandy/proprietary/lib/libqmi_csvt_srvc.so:system/lib/libqmi_csvt_srvc.so \
     vendor/nokia/normandy/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
     vendor/nokia/normandy/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-    vendor/nokia/normandy/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/nokia/normandy/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
     vendor/nokia/normandy/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
     vendor/nokia/normandy/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
